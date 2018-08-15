8711bf1d92c3d98f0546c8e2dd3fbcba43235341
mitohato <mitohato14@gmail.com>
2018-08-15 22:06:15 +0900

    Reformat

--- app/src/main/java/com/ict/mito/andon/ui/notification/NotificationFragment.kt
+++ app/src/main/java/com/ict/mito/andon/ui/notification/NotificationFragment.kt
@@ -1,12 +1,11 @@
 package com.ict.mito.andon.ui.notification
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.ict.mito.andon.R
 
 class NotificationFragment : Fragment() {
@@ -22,7 +21,11 @@ class NotificationFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.notification_fragment, container, false)
+        return inflater.inflate(
+                R.layout.notification_fragment,
+                container,
+                false
+        )
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
--- app/src/main/java/com/ict/mito/andon/ui/union/UnionFragment.kt
+++ app/src/main/java/com/ict/mito/andon/ui/union/UnionFragment.kt
@@ -1,12 +1,11 @@
 package com.ict.mito.andon.ui.union
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.ict.mito.andon.R
 
 class UnionFragment : Fragment() {
@@ -22,7 +21,11 @@ class UnionFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.union_fragment, container, false)
+        return inflater.inflate(
+                R.layout.union_fragment,
+                container,
+                false
+        )
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
