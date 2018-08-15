bcee6745987dca014b73508196bb44fc7c5b1e5c
mitohato <mitohato14@gmail.com>
2018-08-15 21:36:49 +0900

    Replace fragment by navigation id selected

--- app/src/main/java/com/ict/mito/andon/ui/main/MainActivity.kt
+++ app/src/main/java/com/ict/mito/andon/ui/main/MainActivity.kt
@@ -2,30 +2,37 @@ package com.ict.mito.andon.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ict.mito.andon.R
-import kotlinx.android.synthetic.main.activity_main.message
+import com.ict.mito.andon.ui.home.HomeFragment
+import com.ict.mito.andon.ui.notification.NotificationFragment
+import com.ict.mito.andon.ui.union.UnionFragment
 import kotlinx.android.synthetic.main.activity_main.navigation
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fragment: Fragment
 
     private val mOnNavigationItemSelectedListener =
             BottomNavigationView.OnNavigationItemSelectedListener { item ->
                 when (item.itemId) {
                     R.id.navigation_home -> {
-                        message.setText(R.string.title_home)
-                        return@OnNavigationItemSelectedListener true
+                        fragment = HomeFragment.newInstance()
                     }
-                        message.setText(R.string.title_dashboard)
-                        return@OnNavigationItemSelectedListener true
                     R.id.navigation_notification -> {
+                        fragment = NotificationFragment.newInstance()
                     }
-                        message.setText(R.string.title_notifications)
-                        return@OnNavigationItemSelectedListener true
                     R.id.navigation_union -> {
+                        fragment = UnionFragment.newInstance()
                     }
                 }
-                false
+                val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+                transaction.replace(
+                        R.id.container,
+                        fragment
+                ).commit()
+                true
             }
 
     override fun onCreate(savedInstanceState: Bundle?) {
